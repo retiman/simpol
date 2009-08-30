3824937002e70e2edd3f71c878f232a9d00d713c
Min Huang <min.huang@alumni.usc.edu>
2009-08-30 11:06:29 -0700

    Added a test for that bad case

--- src/test/scala/simpol/ParsersSpec.scala
+++ src/test/scala/simpol/ParsersSpec.scala
@@ -13,17 +13,23 @@ object ParsersSpec extends Specification with Parsers {
         case ns: NoSuccess     => assert(false, ns.toString)
       }
     }
-  "parse this sum correctly" in {
-     parseAll(sum, "3*x*y+5*z*q") match {
-       case Success(out, int) => out.toString mustEqual "3*(x^1)*(y^1)+5*(z^1)*(q^1)"
-       case ns: NoSuccess     => assert(false, ns.toString)
-     }
+    "parse this sum correctly" in {
+      parseAll(sum, "3*x*y+5*z*q") match {
+        case Success(out, int) => out.toString mustEqual "3*(x^1)*(y^1)+5*(z^1)*(q^1)"
+        case ns: NoSuccess     => assert(false, ns.toString)
+      }
     }
-  "parse this sum correctly" in {
-     parseAll(sum, "x-y") match {
-       case Success(out, int) => out.toString mustEqual "1*(x^1)+-1*(y^1)"
-       case ns: NoSuccess     => assert(false, ns.toString)
-     }
+    "parse this sum correctly" in {
+      parseAll(sum, "x-y") match {
+        case Success(out, int) => out.toString mustEqual "1*(x^1)+-1*(y^1)"
+        case ns: NoSuccess     => assert(false, ns.toString)
+      }
+    }
+    "parse this sum correctly" in {
+      parseAll(sum, "1-x+6") match {
+        case Success(out, int) => out.toString mustEqual "7+-1*(x^1)"
+        case ns: NoSuccess     => assert(false, ns.toString)
+      }
     }
   }
 
