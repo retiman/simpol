94ac484e26ae42b0edbb788dd39b4a71da636ae7
Min Huang <min.huang@alumni.usc.edu>
2009-09-11 10:39:31 -0700

    Scala's not so consistent here, so I named the args

--- src/main/scala/simpol/Parsers.scala
+++ src/main/scala/simpol/Parsers.scala
@@ -6,30 +6,30 @@ import Term._
 
 trait Parsers extends RegexParsers {
   def sum: Parser[Polynomial] = (
-      term ~ rep(("+" | "-") ~ term) ^^ { case p ~ rest => rest.map(sumAction _).foldLeft(p)(_ + _) }
+      term ~ rep(("+" | "-") ~ term) ^^ { case p ~ rest     => rest.map(sumAction _).foldLeft(p)(_ + _) }
     | term
   )
 
   def term: Parser[Polynomial] = (
-      unary ~ rep("*" ~ term)        ^^ { case p ~ rest => rest.map(_._2).foldLeft(p)(_ * _) }
+      unary ~ rep("*" ~ term)        ^^ { case p ~ rest     => rest.map(_._2).foldLeft(p)(_ * _) }
     | unary
   )
 
   def unary: Parser[Polynomial] = (
-      "_" ~> primary                 ^^ { _ * -1 }
+      "_" ~> primary                 ^^ { case p            => p * -1 }
     | primary
   )
 
   def primary: Parser[Polynomial] = (
-      variable ~ "^" ~ constant      ^^ { case v ~ "^" ~ e => Polynomial(v -> e) }
-    | variable                       ^^ { case v => Polynomial(v -> 1) }
-    | constant                       ^^ { Polynomial(_) }
+      variable ~ "^" ~ constant      ^^ { case v ~ "^" ~ e  => Polynomial(v -> e) }
+    | variable                       ^^ { case v            => Polynomial(v -> 1) }
+    | constant                       ^^ { case c            => Polynomial(c) }
     | "(" ~> sum ~ ")^" ~ constant   ^^ { case s ~ ")^" ~ c => List.make(c, s).reduceLeft(_ * _) }
     | "(" ~> sum <~ ")"
   )
 
-  def variable = """[a-z]""".r       ^^ { Symbol(_) }
-  def constant = """\d+""".r         ^^ { _.toInt }
+  def variable = """[a-z]""".r       ^^ { case s            => Symbol(s) }
+  def constant = """\d+""".r         ^^ { case c            => c.toInt }
 
   def sumAction(result: Any): Polynomial = {
     def coerce(result: Any) = result match { case p: Polynomial => p }
