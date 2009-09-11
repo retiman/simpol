94c2da13318ccfe342e09fee8b13e3df945b5bdb
Min Huang <min.huang@alumni.usc.edu>
2009-09-11 10:05:24 -0700

    Simplified actions even more

--- src/main/scala/simpol/Parsers.scala
+++ src/main/scala/simpol/Parsers.scala
@@ -6,12 +6,12 @@ import Term._
 
 trait Parsers extends RegexParsers {
   def sum: Parser[Polynomial] = (
-      term ~ rep(("+" | "-") ~ term) ^^ sumAction
+      term ~ rep(("+" | "-") ~ term) ^^ { case p ~ rest => rest.map(sumAction _).foldLeft(p)(_ + _) }
     | term
   )
 
   def term: Parser[Polynomial] = (
-      unary ~ rep("*" ~ term)        ^^ { case p ~ rest => rest.map(_ match { case "*" ~ t => t }).foldLeft(p)(_ * _) }
+      unary ~ rep("*" ~ term)        ^^ { case p ~ rest => rest.map(_._2).foldLeft(p)(_ * _) }
     | unary
   )
 
@@ -31,7 +31,8 @@ trait Parsers extends RegexParsers {
   def variable = """[a-z]""".r       ^^ { Symbol(_) }
   def constant = """\d+""".r         ^^ { _.toInt }
 
-  def sumAction(result: ~[Polynomial, List[~[String, Polynomial]]]) = result match {
-    case p ~ rest => rest.map(_ match { case "+" ~ t => t; case "-" ~ t => t * -1 }).foldLeft(p)(_ + _)
+  def sumAction(result: ~[String, Polynomial]) = result match {
+    case "+" ~ p => p
+    case "-" ~ p => p * -1
   }
 }
