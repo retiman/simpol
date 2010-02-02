6bdb76b0d18b6bbb6f54e05b3b9ffc44c5cb32d7
Min Huang <min.huang@alumni.usc.edu>
2010-02-01 23:24:15 -0800

    Don't put : in front of expression

--- src/Main.scala
+++ src/Main.scala
@@ -2,7 +2,7 @@ package simpol
 
 object Main extends Parsers {
   def main(args: Array[String]) = args.foreach { expr =>
-    println("Simplifying: " + expr + ":")
+    println("Simplifying " + expr + ":")
     parseAll(sum, expr) match {
       case Success(out, int) => println("  " + out.simplify.toString)
       case ns: NoSuccess     => println("  " + ns.toString)
