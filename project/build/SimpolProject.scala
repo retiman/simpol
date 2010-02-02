2fecbe6ad33219fcfaf5e05ac5c356217e08d461
Min Huang <min.huang@alumni.usc.edu>
2010-02-01 22:19:26 -0800

    Removed verbose compiler option

--- project/build/SimpolProject.scala
+++ project/build/SimpolProject.scala
@@ -6,7 +6,6 @@ class SimpolProject(info: ProjectInfo) extends DefaultProject(info) {
   override def mainScalaSourcePath = "src"
   override def testScalaSourcePath = "test"
   override def compileOptions      = Deprecation ::
-                                     Verbose ::
                                      Optimize ::
                                      super.compileOptions.toList
   override def packageOptions      = MainClass("simpol.Main") ::
