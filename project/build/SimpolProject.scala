0b51b7a6dec79ef7dfc13e8c93e297d260c1cc16
Min Huang <min.huang@alumni.usc.edu>
2010-02-01 22:57:49 -0800

    Works and builds with SBT

--- project/build/SimpolProject.scala
+++ project/build/SimpolProject.scala
@@ -1,16 +1,24 @@
 import sbt._
 
 class SimpolProject(info: ProjectInfo) extends DefaultProject(info) {
-  override def artifactID          = "simpol"
-  override def outputPath          = "build"
-  override def mainScalaSourcePath = "src"
-  override def testScalaSourcePath = "test"
-  override def compileOptions      = Deprecation ::
-                                     Optimize ::
-                                     super.compileOptions.toList
-  override def packageOptions      = MainClass("simpol.Main") ::
-                                     super.packageOptions.toList
+  // For other configuration options, see
+  // http://code.google.com/p/simple-build-tool/wiki/BuildConfiguration
+  override def artifactID            = "simpol"
+  override def outputPath            = "project" / "target"
+  override def dependencyPath        = "project" / "lib"
+  override def managedDependencyPath = "project" / "lib_managed"
+  override def mainScalaSourcePath   = "src"
+  override def testScalaSourcePath   = "test"
+  override def compileOptions        = Deprecation   ::
+                                       Optimize ::
+                                       super.compileOptions.toList
+  override def packageOptions        = MainClass("simpol.Main") ::
+                                       super.packageOptions.toList
 
+  val scalaToolsRepository           = "Scala-tools Maven2 Repository" at
+                                       "http://scala-tools.org/repo-releases"
+  val lousycoderRepository           = "LousyCoder Maven Repository" at
+                                       "http://maven.lousycoder.com"
   val junit = "junit"     % "junit" % "4.4"   % "test"
-  val specs = "org.specs" % "specs" % "1.4.1" % "test"
+  val specs = "org.specs" % "specs" % "1.4.3" % "test"
 }
